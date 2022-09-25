debef5cfc3ef247a19cbee92a92bb0338bc2bd76
arks143 <93473111+arks143@users.noreply.github.com>
2022-09-25 06:43:05 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,190 +1,182 @@
 const express = require('express'); //Import the express dependency
 const app = express();
-// app.use(express.urlencode());          //Instantiate an express app, the main work horse of this server
 const port = 3000; //Save the port number where your server will be listening
 const axios = require('axios');
+require('dotenv').config();
+const {
+    auth,
+    requiresAuth
+} = require('express-openid-connect');
+const {
+    MongoClient,
+    ServerApiVersion
+} = require('mongodb');
+const uri = process.env.uri;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1
+});
 let response = null;
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+async function createListing(client, newListing) {
+    const result = await client.db("scores").collection("score_rec").insertOne(newListing);
+    console.log(`New listing created with the following id: ${result.insertedId}`);
+}
+
+function createListingProx(email, score,questionid,aid) {
+    createListing(client, {
+        email: email,
+        score: score,
+        score_num: 1,
+        question_id: questionid,
+        aid:aid
+    });
+
+}
+
+async function findOneListingByName(nameOfEmail) {
+    const result = await client.db("scores").collection("score_rec").findOne({
+        email: nameOfEmail
+    });
+
+    if (result) {
+        console.log(`Found a listing in the collection with the name '${nameOfEmail}':`);
+         console.log(result.score_num);
+        return 'true';
+    } else {
+        console.log(`No listings found with the name '${nameOfEmail}'`);
+        return 'false';
+    }
+}
+
+
+async function getOneListingByName(nameOfEmail) {
+    const result = await client.db("scores").collection("score_rec").findOne({
+        email: nameOfEmail
+    });
+
+    if (result) {
+        return result;
+    } else {
+       
+        return 'false';
+    }
+}
+
+
+async function getAllListingByName(nameOfEmail) {
+    console.log('mongodbclient daata2 ');
+    console.log(MongoClient);
+    const cursor = await client.db("scores").collection("score_rec").find({
+        email: {$gte:nameOfEmail}
+    });
+    const results = await cursor.toArray();
+    console.log(results);
+    if (results) {
+        return results;
+    } else {
+        return 'false';
+    }
+}
+
+
+async function updateListingByName(email, updatedListing) {
+    const result = await client.db("scores").collection("score_rec").updateOne(updatedListing);
+    console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+    console.log(`${result.modifiedCount} document(s) was/were updated.`);
+}
+
+
+async function updateListingByName(nameOfListing, updatedListing) {
+    const result = await client.db("scores").collection("score_rec").updateOne({ email: nameOfListing }, { $set: updatedListing });
+    console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+    console.log(`${result.modifiedCount} document(s) was/were updated.`);
+}
+
+
+async function deleteListingsScrapedBeforeDate(email) {
+    const result = await client.db("scores").collection("score_rec").deleteMany({ "email": email });
+    console.log(`${result.deletedCount} document(s) was/were deleted.`);
+}
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: '1PnLMiDyoSE7jKTDkUtb6qkadG_5PCcYmNR9573KtwLcf39BPGk_vdZ_NK0UXdyH',
+    baseURL: 'http://localhost:3000',
+    clientID: 'w2dgBdvE4nhdEEz5pfrQ1yibjjt6Fw71',
+    issuerBaseURL: 'https://dev-ug95wb6k.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 
 //Idiomatic expression in express to route and respond to a client request
-app.get('/', (req, res) => { //get requests to the root ("/") will route here
+app.get('/', requiresAuth(), (req, res) => { //get requests to the root ("/") will route here
+    //res.send(JSON.stringify (req.oidc.user));
     res.sendFile('/index.html', {
         root: __dirname
     }); //server responds by sending the index.html file to the client's browser
     //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 });
-app.get('/visuals', (req, res) => { //get requests to the root ("/") will route here
-    res.sendFile('/visuals.html', {
-        root: __dirname
-    }); //server responds by sending the index.html file to the client's browser
-    //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
-});
-app.get('/main.js', (req, res) => { //get requests to the root ("/") will route here
-    res.sendFile('/main.js', {
-        root: __dirname
-    }); //server responds by sending the index.html file to the client's browser
-    //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
-});
 
-app.get('/style.css', (req, res) => { //get requests to the root ("/") will route here
-    res.sendFile('/style.css', {
-        root: __dirname
-    }); //server responds by sending the index.html file to the client's browser
-    //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
-});
+app.use(express.static('public',{extensions:['html']}));
+app.use(express.static('public/css',{extensions:['css']}));
+app.use(express.static('public/js',{extensions:['js']}));
 
-app.get('/more-info/:country', (req, res) => {
-    new Promise(async (resolve, reject) => {
-        try {
-            response = await axios.get('https://restcountries.com/v3.1/alpha/' + req.params.country + '')
-        } catch (ex) {
-            response = null;
-            // error
-            console.log(ex);
-            reject(ex);
-        }
-        if (response) {
-            // success
-            const json = response.data;
-            res.send(json);
-            resolve(json);
-        }
-    });
-
+app.get('/userdata', (req, res) => { //get requests to the root ("/") will route here
+    const new_data = req.oidc.user;
+    res.send(new_data);
 });
 
 app.get('/answer/:answer', (req, res) => {
-
-        let answer = req.params.answer;
-        const answer_key = ['A distributed ledger of trasactions','A Blockchain','A way for web3 apps to interact with an EVM Blockchain'];
-        const true_false = answer_key.includes(answer);
-
-        if (true_false == 'true'){
-            res.send(true_false);
-        }else{
-            res.send(true_false);
-        }
-
+    let answer = req.params.answer;
+    const answer_key = ['A distributed ledger of trasactions', 'A Blockchain', 'A way for web3 apps to interact with an EVM Blockchain'];
+    const true_false = answer_key.includes(answer);
+    if (true_false == 'true') {
+        res.send(true_false);
+    } else {
+        res.send(true_false);
+    }
 });
 
+// // Handling Get Request '/'
+app.post("/savescore", (req, res) => {
+    console.log('score saved');
+    createListingProx(req.body.email,req.body.score,req.body.qid,req.body.aid);
+});
 
-app.get('/getweather/:latlon', (req, res) => {
-
-    let latlon_array = req.params.latlon.split("_");
-
-    new Promise(async (resolve, reject) => {
-        try {
-            response = await axios.get('https://api.openweathermap.org/data/2.5/weather?lat=' + latlon_array[0] + '&lon=' + latlon_array[1] + '&appid=e17fc3009e8124bd5a89d9d127b79acc')
-        } catch (ex) {
-            response = null;
-            // error
-            console.log(ex);
-            reject(ex);
-        }
-        if (response) {
-            // success
-            const json = response.data;
-            res.send(json);
-            resolve(json);
-        }
+app.get('/check_entry/:email', async (req, res) => { //get requests to the root ("/") will route here
+    //select from db if user is present 
+    const entry_present = await findOneListingByName(req.params.email).then(function(result) {
+        return result;
     });
-
+   
+    res.send('email check');
 });
 
-
-app.get('/time/:latlon', (req, res) => {
-
-    let text = req.params.latlon;
-    const latlon_array = text.split("_");
-    console.log(latlon_array);
-
-    new Promise(async (resolve, reject) => {
-        try {
-            response = await axios.get('https://timeapi.io/api/Time/current/coordinate?latitude='+latlon_array[0] +'&longitude='+ latlon_array[1])
-        } catch (ex) {
-            response = null;
-            // error
-            console.log(ex);
-            reject(ex);
-        }
-        if (response) {
-            // success
-            const json = response.data;
-            res.send(json);
-            resolve(json);
-        }
+app.get('/remove_score', async (req, res) => { //get requests to the root ("/") will route here
+    //select from db if user is present 
+    const entry_present = await deleteListingsScrapedBeforeDate(req.oidc.user.email).then(function(result) {
+        return result;
     });
-
+    console.log('Answers Removed')
+    res.send('Answers Removed');
 });
 
-
-app.get('/bankholidays/:country', (req, res) => {
-
-    let text = req.params.country;
-
-    new Promise(async (resolve, reject) => {
-        try {
-            response = await axios.get('https://date.nager.at/api/v3/PublicHolidays/2022/' + req.params.country)
-        } catch (ex) {
-            response = null;
-            // error
-
-            reject(ex);
-        }
-        if (response) {
-            // success
-            const json = response.data;
-            //   var jsonData = {};
-            //   var i = 0;
-
-            // json.forEach(function(item, index) {
-            //         // console.log(item['date']);
-            //         // console.log(index);
-            //         // console.log(new Date(item['date']).getTime());
-            //         var today = new Date().getTime();
-            //         var item_timestamp = new Date(item['date']).getTime();
-            //        console.log(today);
-            //            console.log(item_timestamp);
-            //          if( item_timestamp > today ){
-            //           console.log('hello world'+i);
-            //           jsonData = json[i];
-            //           console.log(json[i]);
-            //         i++;
-            //          }
-            //          //console.log(jsonData);
-                        
-            //         });
- 
-            res.send(json);
-            resolve(json);
-        }
+app.get('/getscore', async (req, res) => { //get requests to the root ("/") will route here
+    //select from db if user is present 
+    const total = await getAllListingByName(req.oidc.user.email).then(function(result) {
+        return result;
     });
-
+    console.log('Total')
+    console.log(total)
+    res.send(total);
 });
-
-
-app.get('/latlon/:place', (req, res) => {
-
-    new Promise(async (resolve, reject) => {
-        try {
-            response = await axios.get('http://api.openweathermap.org/geo/1.0/direct?q=' + req.params.place + '&limit=5&appid=e17fc3009e8124bd5a89d9d127b79acc')
-        } catch (ex) {
-            response = null;
-            // error
-
-            reject(ex);
-        }
-        if (response) {
-            // success
-            const json = response.data;
-            res.send(json);
-            resolve(json);
-        }
-    });
-
-});
-
-
 
 app.listen(port, () => { //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${port}`);
---
+++ public/js/main.js
@@ -0,0 +1,328 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+const finalScore = document.getElementById('final-score')
+const blockchain = document.getElementById('blockchain')
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+
+async function getUserData() {
+    let url = '/userdata';
+    try {
+        let res = await fetch(url);
+        let data = await res.json();
+        return data.email;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+async function getScoreTotal() {
+    let url = '/getscore';
+    try {
+        let res = await fetch(url);
+        let data = await res.json();
+        var i = 1;
+        data.forEach(element => {
+            i++;
+        });
+        return i;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+async function checkEntry(usermail) {
+    let url = '/check_entry/' + usermail + '';
+    try {
+        let res = await fetch(url);
+        //let res2 = await res.json();
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+async function removeScore() {
+    let url = '/remove_score';
+    try {
+        let res = await fetch(url);
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+function insertData(user_mail, correct, questionid, aid) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    myHeaders.append("Cookie", "_auth_verification=%7B%22nonce%22%3A%22gk45MVicoWYQxrU4S0wtiGsuTal-7-hCa80pOjRz5e4%22%2C%22state%22%3A%22eyJyZXR1cm5UbyI6Imh0dHA6Ly9sb2NhbGhvc3Q6MzAwMCJ9%22%7D.V26yoqMEXeQWYF_VkG2J3WJA6FeAifkt_Cso_DmaN8I");
+    var urlencoded = new URLSearchParams();
+
+    urlencoded.append("email", user_mail);
+    urlencoded.append("score", correct);
+    urlencoded.append("qid", questionid);
+    urlencoded.append("aid", aid);
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: 'follow'
+    };
+
+    fetch("/savescore", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
+
+
+async function saveScore(correct, questions, aid) {
+    const user_mail = await getUserData();
+    await insertData(user_mail, correct, questions, aid);
+}
+
+
+async function startGame() {
+    const entry = await checkEntry(await getUserData());
+
+    if (entry) {
+        startButton.innerText = 'Re-Take Test';
+        removeScore();
+    }
+
+    startButton.classList.add('hide')
+    finalScore.classList.add('hide')
+    blockchain.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    //const consolee
+    console.log(question.qnumber); //load number to frontend
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+
+async function selectAnswer(e) {
+    const selectedButton = e.target
+    const aid = selectedButton;
+    console.log(aid);
+    var correct = '';
+    var answer_protect = selectedButton.innerText;
+    let url = '/answer/' + answer_protect + '';
+
+    try {
+        let res = await fetch(url);
+        let res2 = await res.json();
+        correct = res2.toString();
+        if (correct == 'true') {
+            console.log(selectedButton);
+            // console.log(shuffledQuestions[currentQuestionIndex].answers[0].aid);
+            var i = 0;
+            Array.from(answerButtonsElement.children).forEach(button => {
+                shuffledQuestions[currentQuestionIndex].answers[i].aid;
+                selectedButton.dataset.aid = shuffledQuestions[currentQuestionIndex].answers[i].aid
+            })
+            saveScore(correct, shuffledQuestions[currentQuestionIndex].qnumber, selectedButton.dataset.aid).then(function(result) {});
+        }
+        if (correct == 'false') {
+            correct = 'wrong';
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    //set data to correct or wrong
+    selectedButton.dataset.correct = correct;
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+
+    //disable answer buttons could put onto a function 
+    const button = document.getElementById('answer-buttons');
+    button.getElementsByClassName('btn')
+    const answer_buttons = button.getElementsByClassName('btn');
+
+    for (const element of answer_buttons) {
+        element.setAttribute('disabled', '')
+    }
+
+
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+
+        const score = document.getElementsByClassName('score');
+        score[0].innerText = await getScoreTotal();
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+        finalScore.classList.remove('hide')
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct == 'true') {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+async function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+const questions = [{
+        question: 'What is a blockchain?',
+        qnumber: 'q1',
+        answers: [{
+                text: 'A distributed ledger of trasactions',
+                aid: '01'
+            },
+            {
+                text: 'A database',
+                aid: '02'
+            }
+        ]
+    },
+    {
+        question: 'What is a Bitcoin',
+        qnumber: 'q2',
+        answers: [{
+                text: 'Fiat Currency',
+                aid: '01'
+            },
+            {
+                text: 'An NFT',
+                aid: '02'
+            },
+            {
+                text: 'A Crypto Currency',
+                aid: '03'
+            },
+            {
+                text: 'A type of food',
+                aid: '04'
+            }
+        ]
+    },
+    {
+        question: 'What is Crypto used for?',
+        qnumber: 'q3',
+        answers: [{
+                text: 'Transfer of wealth',
+                aid: '01'
+            },
+            {
+                text: 'Hacking',
+                aid: '02'
+            },
+            {
+                text: 'Google Search',
+                aid: '03'
+            },
+            {
+                text: 'IDK',
+                aid: '04'
+            }
+        ]
+    },
+    {
+        question: 'What invented Bitcoin?',
+        qnumber: 'q4',
+        answers: [{
+                text: 'Satoshi Nakamoto',
+                aid: '01'
+            },
+            {
+                text: 'Bill Clinton',
+                aid: '02'
+            }
+        ]
+    },
+    {
+        question: 'What is Ass coin',
+        qnumber: 'q5',
+        answers: [{
+                text: 'Fiat Currency',
+                aid: '01'
+            },
+            {
+                text: 'An NFT',
+                aid: '02'
+            },
+            {
+                text: 'A Coin you should not buy ',
+                aid: '03'
+            },
+            {
+                text: 'A type of food',
+                aid: '04'
+            }
+        ]
+    },
+    {
+        question: 'What was the price of Bitcoin September 21 2017',
+        qnumber: 'q6',
+        answers: [{
+                text: '12000',
+                aid: '01'
+            },
+            {
+                text: 'who cares',
+                aid: '02'
+            },
+            {
+                text: '$67884',
+                aid: '03'
+            },
+            {
+                text: '42',
+                aid: '04'
+            }
+        ]
+    }
+
+]
